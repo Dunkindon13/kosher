cfa611f42405f4549948982c34b57853809f8a04
Meir Donath <donathme@gmail.com>
2019-03-13 13:38:31 -0400

    Added Material Card

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'kosher';
+
+export class AppComponent implements OnInit {
+  cardTitle = 'Kosher App';
+  cardSub = 'Kosher Info';
+  cardContent = 'A future list of Kosher things.';
+
+  // Init method
+  ngOnInit(): void {
+    this.cardTitle = 'Word!!';
+  }
 }
+
+
